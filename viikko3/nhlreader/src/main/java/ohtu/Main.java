428d7f8f8dd340fd0ed1f5aff9890aba6c259607
Qubelka <42945433+qubelka@users.noreply.github.com>
2019-11-11 23:34:42 +0200

    Add dependencies to gradle.build

--- viikko3/nhlreader/src/main/java/ohtu/Main.java
+++ viikko3/nhlreader/src/main/java/ohtu/Main.java
@@ -1,14 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ohtu;
 
-/**
- *
- * @author mluukkai
- */
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+import com.google.gson.Gson;
+import org.apache.http.client.fluent.Request;
+
 public class Main {
-    
+    public static void main(String[] args) throws IOException {
+        String url = "https://nhlstatisticsforohtu.herokuapp.com/players";
+
+        String bodyText = Request.Get(url).execute().returnContent().asString();
+
+//        System.out.println("json-muotoinen data:");
+//        System.out.println(bodyText);
+
+        Gson mapper = new Gson();
+        Player[] players = mapper.fromJson(bodyText, Player[].class);
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("EEE MMM dd hh:mm:ss z yyyy");
+        LocalDateTime now = LocalDateTime.now();
+        ZoneId zoneId = ZoneId.of("Europe/Helsinki");
+        ZonedDateTime zdt = ZonedDateTime.of(now, zoneId);
+        System.out.println("Players from FIN " + dtf.format(zdt));
+        for (Player player : players) {
+            if (player.getNationality().equals("FIN")) {
+                System.out.println(player);
+            }
+        }
+    }
 }
--- viikko3/nhlreader/src/main/java/ohtu/Player.java
+++ viikko3/nhlreader/src/main/java/ohtu/Player.java
@@ -2,7 +2,29 @@
 package ohtu;
 
 public class Player {
+    private int goals;
+    private int assists;
     private String name;
+    private int penalties;
+    private String team;
+    private String nationality;
+    private String birthdate;
+
+    public int getGoals() {
+        return goals;
+    }
+
+    public void setGoals(int goals) {
+        this.goals = goals;
+    }
+
+    public int getAssists() {
+        return assists;
+    }
+
+    public void setAssists(int assists) {
+        this.assists = assists;
+    }
 
     public void setName(String name) {
         this.name = name;
@@ -12,9 +34,41 @@ public class Player {
         return name;
     }
 
+    public int getPenalties() {
+        return penalties;
+    }
+
+    public void setPenalties(int penalties) {
+        this.penalties = penalties;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
     @Override
     public String toString() {
-        return name;
+        return name + " team " + team + " goals " +
+                goals + " assists " + assists;
     }
-      
 }
