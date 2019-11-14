59994b68d8ae44307aae2f55da19178f03e17d6e
Qubelka <42945433+qubelka@users.noreply.github.com>
2019-11-14 12:07:39 +0200

    Week 3 task 8: web-selaimen simulointi

--- viikko3/WebLogin/src/main/java/ohtu/Tester.java
+++ viikko3/WebLogin/src/main/java/ohtu/Tester.java
@@ -5,34 +5,50 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.Random;
+
 public class Tester {
 
     public static void main(String[] args) {
         WebDriver driver = new FirefoxDriver();
 
         driver.get("http://localhost:4567");
-        
+        Random r = new Random();
         sleep(2);
         
         WebElement element = driver.findElement(By.linkText("login"));
         element.click();
-
         sleep(2);
 
-        element = driver.findElement(By.name("username"));
-        element.sendKeys("pekka");
-        element = driver.findElement(By.name("password"));
-        element.sendKeys("akkep");
+        // 1) Right username, wrong password
+        driver.findElement(By.name("username")).sendKeys("pekka");
+        driver.findElement(By.name("password")).sendKeys("p");
         element = driver.findElement(By.name("login"));
-        
         sleep(2);
         element.submit();
+        sleep(2);
+        driver.findElement(By.linkText("back to home")).click();
+        sleep(2);
 
-        sleep(3);
-        
+        // 2) Registering a new user
+        driver.findElement(By.linkText("register new user")).click();
+        sleep(2);
+        driver.findElement(By.name("username")).sendKeys("arto"+r.nextInt(100000));
+        sleep(2);
+        driver.findElement(By.name("password")).sendKeys("sa1asana");
+        driver.findElement(By.name("passwordConfirmation")).sendKeys("sa1asana");
+        sleep(2);
+        driver.findElement(By.name("signup")).submit();
+        sleep(2);
+
+        // 3) Log out after account creation
+        driver.findElement(By.linkText("continue to application mainpage")).click();
+        sleep(2);
+        driver.findElement(By.linkText("logout")).click();
+        sleep(2);
         driver.quit();
     }
-    
+
     private static void sleep(int n){
         try{
             Thread.sleep(n*1000);
