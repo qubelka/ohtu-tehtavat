b32a4c543afef2c83801b134cfddb36a8b81e49b
Qubelka <42945433+qubelka@users.noreply.github.com>
2019-11-19 17:32:18 +0200

    Week 4 task 4: Mockito, osa 4

--- viikko4/Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
+++ viikko4/Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -2,6 +2,8 @@ package ohtu.verkkokauppa;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import static org.mockito.Mockito.*;
 
 public class KauppaTest {
@@ -20,17 +22,57 @@ public class KauppaTest {
     }
 
     private void alusta() {
-        when(viite.uusi()).thenReturn(42);
+        when(viite.uusi()).thenAnswer(new Answer() {
+            private int viiteNumero = 42;
+
+            public Object answer(InvocationOnMock invocation) {
+                return viiteNumero++;
+            }
+        });
         when(varasto.saldo(1)).thenReturn(10);
         when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
 
-        when(varasto.saldo(2)).thenReturn(5);
+        when(varasto.saldo(2)).thenReturn(1);
         when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "Analyysia reaaliluvuilla", 30));
 
         when(varasto.saldo(3)).thenReturn(0);
         when(varasto.haeTuote(3)).thenReturn(new Tuote(3, "Databases illuminated", 90));
 
         k.aloitaAsiointi();
+    }
+
+    @Test
+    public void aloitaAsiointiNollaaEdellisenOstoksenTiedot() {
+        k.lisaaKoriin(1);
+        k.aloitaAsiointi();
+        k.lisaaKoriin(2);
+        k.tilimaksu("pekka", "12345");
+        verify(pankki).tilisiirto(anyString(), anyInt(), anyString(), anyString(), eq(30));
+    }
+
+    @Test
+    public void uusiViitenumeroJokaiselleMaksutapahtumalle() {
+        k.lisaaKoriin(1);
+        k.tilimaksu("pekka", "12345");
+
+        k.aloitaAsiointi();
+        k.lisaaKoriin(2);
+        k.tilimaksu("p", "00000");
+
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.tilimaksu("z", "23232");
+
+        verify(pankki).tilisiirto(anyString(), eq(44), anyString(), anyString(), anyInt());
+    }
+
+    @Test
+    public void poistaKoristaPoistaaTuotteenOstoskorista() {
+        Tuote t = varasto.haeTuote(1);
+        k.lisaaKoriin(1);
+        verify(varasto, times(1)).otaVarastosta(t);
+        k.poistaKorista(1);
+        verify(varasto, times(1)).palautaVarastoon(t);
     }
 
     @Test
